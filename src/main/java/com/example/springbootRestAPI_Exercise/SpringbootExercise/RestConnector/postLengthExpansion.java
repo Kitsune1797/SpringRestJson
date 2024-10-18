f750180fe0c2eda7688823d5956b8e34254dfddf
Kitsune1797 <kwoksang1011@gmail.com>
2024-10-19 01:18:34 +0800

    FOURTH COMMIT - TASK 3 1. Added a new java class to perform the algorithm that calculates the length of each title, and add the result into a new field in each post 2. file affected: <new> postLengthExpansion.java 3. algorithm calculation part is in the method calc_Length

---
+++ src/main/java/com/example/springbootRestAPI_Exercise/SpringbootExercise/RestConnector/postLengthExpansion.java
@@ -0,0 +1,31 @@
+package com.example.springbootRestAPI_Exercise.SpringbootExercise.RestConnector;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class postLengthExpansion {
+
+    @RequestMapping("/des3")
+    public List<Map<String, Object>> getErrorJsonList() {
+        String requiredAPI = contentGet.getjson();
+        RestTemplate restData = new RestTemplate();
+        ResponseEntity<List> jsonContent = restData.getForEntity(requiredAPI, List.class);
+        List<Map<String, Object>> postContent = jsonContent.getBody();
+        calc_Length(postContent);
+        return postContent;
+    }
+
+    public void calc_Length(List<Map<String, Object>> jsonLContent) {
+        for (Map<String, Object> currPost : jsonLContent) {
+            String currTitle = currPost.get("title").toString();
+            int currLength = currTitle.length();
+            currPost.put("titleLength", currLength);
+        }
+    }
+}
